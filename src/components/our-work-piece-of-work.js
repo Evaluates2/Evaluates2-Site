d3579f1c2c46313b66dc31eeb32a4bc3e1c9b9c1
JimTheMan <mrdotjim@gmail.com>
2019-10-04 23:11:51 -0400

    out work page

---
+++ src/components/our-work-piece-of-work.js
@@ -0,0 +1,169 @@
+import styled from '@emotion/styled';
+import ProgressTrackingImage from './img-components/progress-tracking.img';
+import StaffAugmentationImage from './img-components/staff-augmentation.img';
+import ProjectKickoffImage from './img-components/project-kickoff.img';
+import Link from 'gatsby-link';
+import KateFromHrScreenshotImage from './img-components/kate-from-hr-screenshot.img';
+import JimTwitterScreenshotImage from './img-components/jim-twitter-screenshot.img';
+
+const StyledOurWorkPieceOfWork = styled.div`
+  .container {
+    flex: 1;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    padding: 50px;
+    flex: 1;
+    width: 100%;
+    height: 100%;
+    flex-direction: row-reverse;
+
+    margin: 0;
+
+    @media only screen and (max-width: 960px) {
+      align-items: center;
+      font-size: 18px;
+      line-height: 31px;
+      flex-direction: column;
+    }
+  }
+
+  p {
+    font-size: 16px;
+    line-height: 29px;
+    letter-spacing: 0.75px;
+    font-family: 'e2-Raleway';
+    color: #4a4a4a;
+
+    @media only screen and (max-width: 900px) {
+      font-size: 19px;
+      line-height: 32px;
+    }
+  }
+
+  div {
+    align-content: center;
+    text-align: center;
+    flex: 1;
+
+    .img {
+      flex: 1;
+      margin: 35px;
+      object-fit: contain;
+      width: 100%;
+      height: 100%;
+    }
+
+    a {
+      font-size: 22px;
+      margin: 10px 0 10px;
+      display: block;
+    }
+    .text-container {
+      flex: 1;
+      text-align: left;
+    }
+  }
+
+  h1 {
+    font-size: 43px;
+    letter-spacing: 0.5px;
+    font-family: 'e2-Raleway-Extra-Bold';
+  }
+
+  h2 {
+    font-size: 24px;
+    letter-spacing: 0.25px;
+    font-family: 'e2-Raleway';
+  }
+
+  .one {
+    background-color: lightgreen;
+    flex: 1;
+  }
+
+  .two {
+    flex: 1;
+    background-color: lightskyblue;
+  }
+
+  .reversed {
+    flex-direction: row;
+
+    @media only screen and (max-width: 960px) {
+      flex-direction: column;
+    }
+  }
+
+  button {
+    background-image: linear-gradient(90deg, #ff7d00, #e0d950);
+    transition: opacity 0.2s ease;
+    color: #fff;
+    font-size: 15px;
+    line-height: 24px;
+    font-family: 'e2-Raleway-Semi-Bold';
+    padding: 10px 26px;
+    border-radius: 4px;
+    border: none;
+    max-width: 200px;
+    cursor: pointer;
+    transition-property: opacity;
+    transition-duration: 0.2s;
+    transition-timing-function: ease;
+    transition-delay: 0s;
+  }
+`;
+
+const OurWorkPieceOfWork = ({
+  title,
+  header1,
+  header1link,
+  paragraph1,
+  header2,
+  paragraph2,
+  imgName,
+  imgOnLeft,
+  buttonText,
+  buttonLinkTo,
+  children,
+}) => {
+  const getImage = imgName => {
+    switch (imgName) {
+      case 'KATE_FROM_HR':
+        return (
+          <a href={header1link}>
+            <KateFromHrScreenshotImage />
+          </a>
+        );
+        
+        case 'JIM_TWITTER':
+          return (<a href={header1link}>
+             <JimTwitterScreenshotImage />
+          </a>);
+
+      case 'WOG_BLOG':
+        return <JimTwitterScreenshotImage />;
+    }
+  };
+
+  return (
+    <StyledOurWorkPieceOfWork>
+      <div className={`container ${imgOnLeft ? 'reversed' : ''}`}>
+        <div className="img">{getImage(imgName)}</div>
+        <div className="text-container">
+          <h1>{title}</h1>
+          <a href={header1link}>{header1}</a>
+          <p>{paragraph1}</p>
+          <h2>{header2}</h2>
+          <p>{paragraph2}</p>
+          {children}
+          <a href={buttonLinkTo}>
+            <button>{buttonText}</button>
+          </a>
+        </div>
+      </div>
+    </StyledOurWorkPieceOfWork>
+  );
+};
+
+export default OurWorkPieceOfWork;
--- src/pages/our-work.js
+++ src/pages/our-work.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import Global from "../components/Global"
+import Global from '../components/Global';
 import Layout from '../components/layout';
 import styled from '@emotion/styled';
 import JoinOurTeamCornerBtn from './../components/generic-reusable-components/join-our-team-corner-btn';
-import PersonImage from '../components/img-components/person-img.img';
 import Link from 'gatsby-link';
 import TeamMemberBlock from './../components/generic-reusable-components/team-member-block';
+import StackableImageAndTextDuo from '../components/generic-reusable-components/stackable-image-and-text-duo';
+import StackableColumnsContainer from '../components/generic-reusable-components/stackable-columns-container';
+import OurWorkPieceOfWork from '../components/our-work-piece-of-work';
+import ChangeGearsBlock from '../components/generic-reusable-components/change-gears-block';
 
 const OurWorkHero = styled.div`
   h1 {
@@ -50,45 +53,40 @@ const OurWorkHero = styled.div`
 `;
 
 const CenteredVideo = styled.div`
-    text-align: center;
-    width: 100%;
-    display: flex;
-    flex: 1;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
+  text-align: center;
+  width: 100%;
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
 `;
 
 const BigSectionTitle = styled.div`
+  padding: 0 100px;
+  min-height: 500px;
 
-padding: 0 100px;
-min-height: 500px;
-  
-h1 {
+  h1 {
     font-size: 80px;
     line-height: 100px;
     font-family: 'e2-Raleway-Extra-Bold';
     color: #000032;
   }
-  
+
   p {
     font-size: 30px;
     line-height: 45px;
-
   }
-
-
 `;
 
 const NiceQuoteSection = styled.div`
-
   padding: 0 100px;
 
   h1 {
     margin-bottom: 0;
     font-size: 34px;
     line-height: 60px;
-    font-family: 'e2-Raleway-Bold';
+    font-family: 'e2-Raleway-Semi-Bold-Italic';
   }
 
   p {
@@ -99,7 +97,6 @@ const NiceQuoteSection = styled.div`
   }
 
   min-height: 300px;
-
 `;
 
 const OurWorkBlurb = styled.div`
@@ -110,15 +107,10 @@ const OurWorkBlurb = styled.div`
     padding: 10px 40px 230px;
     color: #333333;
     text-align: justify;
-    
   }
 `;
 
-
-
-
 const OurTeamSection = styled.div`
-
   padding: 0 100px;
 
   h1 {
@@ -130,7 +122,6 @@ const OurTeamSection = styled.div`
   }
 
   .our-team-grid {
-
     display: grid;
     grid-gap: 35px;
     justify-content: space-around;
@@ -139,55 +130,40 @@ const OurTeamSection = styled.div`
     grid-template-columns: auto auto auto auto auto auto auto;
 
     @media only screen and (max-width: 1350px) {
-    grid-template-columns: auto auto auto auto auto auto;
-        
-      }
+      grid-template-columns: auto auto auto auto auto auto;
+    }
 
     @media only screen and (max-width: 1160px) {
-    grid-template-columns: auto auto auto auto auto;
-        
-      }
+      grid-template-columns: auto auto auto auto auto;
+    }
     @media only screen and (max-width: 990px) {
-    grid-template-columns: auto auto auto auto;
-        
-      }
+      grid-template-columns: auto auto auto auto;
+    }
 
     @media only screen and (max-width: 810px) {
-    grid-template-columns: auto auto auto;
-
-      }
+      grid-template-columns: auto auto auto;
+    }
     @media only screen and (max-width: 610px) {
-    grid-template-columns: auto auto ;
-
-      }
+      grid-template-columns: auto auto;
+    }
     @media only screen and (max-width: 430px) {
-    grid-template-columns: auto;
-
-      }
-      
+      grid-template-columns: auto;
+    }
 
     .team-member-cell {
-
       .team-member-img {
-
       }
 
       h2 {
-
       }
 
       p {
-
       }
-
     }
-
   }
-
 `;
 
 const WhatWeveBuiltSection = styled.div`
-
   padding: 0 100px;
 
   h1 {
@@ -198,72 +174,62 @@ const WhatWeveBuiltSection = styled.div`
   }
 
   p {
-
-    opacity: .8;
+    opacity: 0.8;
     font-size: 30px;
     line-height: 40px;
     font-family: 'e2-Raleway';
 
     padding: 20px 0;
-
   }
-
-
-
 `;
 
 const PinkBoxesContainer = styled.div`
-
   padding: 0 100px;
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-around;
 
-
   @media only screen and (max-width: 990px) {
-  flex-direction: column;
-
+    flex-direction: column;
   }
-
 `;
 
 const PinkBox = styled.div`
-    display: flex;
-    width: 35vw;
-    height: 35vw;
-    min-width: 250px;
-    min-height: 250px;
-    max-width: 450px;
-    max-height: 450px;
-    margin: 20px 20px;
-    padding: 30px;
-    -webkit-flex-direction: column;
-    -ms-flex-direction: column;
-    flex-direction: column;
-    -webkit-box-pack: start;
-    -webkit-justify-content: flex-start;
-    -ms-flex-pack: start;
-    justify-content: flex-start;
-    -webkit-box-align: start;
-    -webkit-align-items: flex-start;
-    -ms-flex-align: start;
-    align-items: flex-start;
-    background-image: linear-gradient(51deg,#f0f,#efe51a);
-    color: #fff;
-    border-radius: 4px;
+  display: flex;
+  width: 35vw;
+  height: 35vw;
+  min-width: 250px;
+  min-height: 250px;
+  max-width: 450px;
+  max-height: 450px;
+  margin: 20px 20px;
+  padding: 30px;
+  -webkit-flex-direction: column;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  -webkit-box-pack: start;
+  -webkit-justify-content: flex-start;
+  -ms-flex-pack: start;
+  justify-content: flex-start;
+  -webkit-box-align: start;
+  -webkit-align-items: flex-start;
+  -ms-flex-align: start;
+  align-items: flex-start;
+  background-image: linear-gradient(51deg, #f0f, #efe51a);
+  color: #fff;
+  border-radius: 4px;
 
-    h1 {
-      font-size: 35px;
-      margin: 20px auto 30px 0;
-      text-align: left;
-      min-height: 85px;
-    }
+  h1 {
+    font-size: 35px;
+    margin: 20px auto 30px 0;
+    text-align: left;
+    min-height: 85px;
+  }
 
-    p {
-      font-size: 24px;
-
-    }
+  p {
+    font-size: 24px;
+  }
 `;
 
 const OurWorkPage = () => (
@@ -273,25 +239,129 @@ const OurWorkPage = () => (
       <br />
       <br />
 
-
       <OurWorkHero>
         <h1>Our&nbsp;Work</h1>
-        <p>We don't just say we've built awesome things- we've been doing it for a while...</p>
+        <p>
+          We don't just say we've built awesome things- we've been doing it for
+          a while...
+        </p>
       </OurWorkHero>
-
 
       <BigSectionTitle>
         <h1>Success Stories</h1>
-        <p>A few of the previous projecs we've succesfully helped clients launch.</p>
+        <p>
+          A few of the previous projecs we've succesfully helped clients launch.
+        </p>
       </BigSectionTitle>
+
+      {/* <StackableColumnsContainer> */}
+      <OurWorkPieceOfWork
+        title="Kate From HR"
+        header1="www.katefromhr.com"
+        header1link="https://www.katefromhr.com"
+        paragraph1="Kate from HR is a tool for making teams better. A fun and easy-to-use Slack-integrated chatbot collected anonymous feedback that can be public or private (specified by the user). This is then aggregated and visualized in a web dashboard."
+        header2=""
+        paragraph2=""
+        imgName="KATE_FROM_HR"
+        imgOnLeft={true}
+        buttonText="Learn More"
+        buttonLinkTo="https://www.katefromhr.com/why-kate"
+      >
+        <ul>
+          <li>Monthly Pricing Based on Company Size.</li>
+          <li>Live & In Production Since Sept. 2019!</li>
+          <li>Built with Node.js, Angular 7, AWS Lambda, MongoDB...</li>
+        </ul>
+      </OurWorkPieceOfWork>
+
+      <br />
+
+      <OurWorkPieceOfWork
+        title="Social Engagement Serverless Functions"
+        header1="www.twitter.com/WebWhizJim"
+        header1link="https://www.twitter.com/WebWhizJim"
+        paragraph1="Kate from HR is a tool for making teams better. A fun and easy-to-use Slack-integrated chatbot collected anonymous feedback that can be public or private (specified by the user). This is then aggregated and visualized in a web dashboard."
+        header2=""
+        paragraph2=""
+        imgName="JIM_TWITTER"
+        imgOnLeft={false}
+        buttonText="Tweet Jim!"
+        buttonLinkTo="https://www.twitter.com/WebWhizJim"
+      >
+        <ul>
+          <li>Extremely Cheap & Quick to Run & Maintain.</li>
+          <li>Aquired Thousands of Total Likes & Followers for @WebWhizJim and other private client accounts.</li>
+          <li>Written for AWS Lambda Twice Using Both Es6 JavaScript and ClojureScript.</li>
+        </ul>
+      </OurWorkPieceOfWork>
+
+      <OurWorkPieceOfWork
+        title="WoJ Blog"
+        header1="www.wisdomofjim.com"
+        header1link="https://www.wisdomofjim.com"
+        paragraph1="Kate from HR is a tool for making teams better. A fun and easy-to-use Slack-integrated chatbot collected anonymous feedback that can be public or private (specified by the user). This is then aggregated and visualized in a web dashboard."
+        header2=""
+        paragraph2=""
+        imgName="JIM_TWITTER"
+        imgOnLeft={false}
+        buttonText="Tweet Jim!"
+        buttonLinkTo="https://www.wisdomofjim.com"
+      >
+        <ul>
+          <li>Extremely Cheap & Quick to Run & Maintain.</li>
+          <li>Aquired Thousands of Total Likes & Followers for @WebWhizJim and other private client accounts.</li>
+          <li>Written for AWS Lambda Twice Using Both Es6 JavaScript and ClojureScript.</li>
+        </ul>
+      </OurWorkPieceOfWork>
+      {/* </StackableColumnsContainer> */}
 
       <BigSectionTitle>
         <h1>Open Source</h1>
-        <p>Evaluates2 engineers are not only great contractors; we are also contributors back to the open source community!</p>
+        <p>
+          Evaluates2 engineers are not only great contractors; we are also
+          contributors back to the open source community!
+        </p>
       </BigSectionTitle>
 
+      <OurWorkPieceOfWork
+        title="Titanium Lambda"
+        header1="www.wisdomofjim.com"
+        header1link="https://www.wisdomofjim.com"
+        paragraph1="Kate from HR is a tool for making teams better. A fun and easy-to-use Slack-integrated chatbot collected anonymous feedback that can be public or private (specified by the user). This is then aggregated and visualized in a web dashboard."
+        header2=""
+        paragraph2=""
+        imgName="JIM_TWITTER"
+        imgOnLeft={false}
+        buttonText="Tweet Jim!"
+        buttonLinkTo="https://www.wisdomofjim.com"
+      >
+        <ul>
+          <li>Extremely Cheap & Quick to Run & Maintain.</li>
+          <li>Aquired Thousands of Total Likes & Followers for @WebWhizJim and other private client accounts.</li>
+          <li>Written for AWS Lambda Twice Using Both Es6 JavaScript and ClojureScript.</li>
+        </ul>
+      </OurWorkPieceOfWork>
 
-      <CenteredVideo>
+      <OurWorkPieceOfWork
+        title="Sample Projects & Workshops"
+        header1="www.wisdomofjim.com"
+        header1link="https://www.wisdomofjim.com"
+        paragraph1="Kate from HR is a tool for making teams better. A fun and easy-to-use Slack-integrated chatbot collected anonymous feedback that can be public or private (specified by the user). This is then aggregated and visualized in a web dashboard."
+        header2=""
+        paragraph2=""
+        imgName="JIM_TWITTER"
+        imgOnLeft={false}
+        buttonText="Tweet Jim!"
+        buttonLinkTo="https://www.wisdomofjim.com"
+      >
+        <ul>
+          <li>Extremely Cheap & Quick to Run & Maintain.</li>
+          <li>Aquired Thousands of Total Likes & Followers for @WebWhizJim and other private client accounts.</li>
+          <li>Written for AWS Lambda Twice Using Both Es6 JavaScript and ClojureScript.</li>
+        </ul>
+      </OurWorkPieceOfWork>
+
+      {/* <CenteredVideo>
         <iframe
           width="560"
           height="315"
@@ -300,90 +370,62 @@ const OurWorkPage = () => (
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         ></iframe>
-      </CenteredVideo>
+      </CenteredVideo> */}
 
-      <OurWorkBlurb>
+      {/* <OurWorkBlurb>
         <p>
-          "At Evaluates2 we believe in the integrity of our code and are passionate
-            ourWork always learning. We work tirelessly to help our clients build the
-            best products possible and to contribute back to the software development
-            community; because it's not just our job, it's our passion."
-      </p>
-      </OurWorkBlurb>
+          "At Evaluates2 we believe in the integrity of our code and are
+          passionate ourWork always learning. We work tirelessly to help our
+          clients build the best products possible and to contribute back to the
+          software development community; because it's not just our job, it's
+          our passion."
+        </p>
+      </OurWorkBlurb> */}
 
-   
-
+<br/>
+<br/>
+<br/>
+<br/>
       <NiceQuoteSection>
         <h1>
-          "They are not only razor sharp developers, they are excellent people as well."
+          "Although we've <span className="empashized">accomplished</span> a lot, I still wake up every day <span className="empashized">hungry</span> and ready to take on new and <span className="empashized">exciting</span> software development <span className="empashized">challenges</span>."
         </h1>
-        <p>
-          Artspool
-        </p>
+        <p>- Founder & Principal Engineer Jim Lynch</p>
       </NiceQuoteSection>
 
-      <OurTeamSection>
-        <h1>Our Team</h1>
-        <div className="our-team-grid">
+      <ChangeGearsBlock headerText='Find out more about how we work...' linkText='Product Management' linkTo='/product-management'></ChangeGearsBlock>
 
-          <TeamMemberBlock name="Joe Leo" linkTo="joe-leo" role="Ceo/Co-founder"></TeamMemberBlock>
-          <TeamMemberBlock name="Joe Leo" linkTo="joe-leo" role="Ceo/Co-founder"></TeamMemberBlock>
-          <TeamMemberBlock name="Joe Leo" linkTo="joe-leo" role="Ceo/Co-founder"></TeamMemberBlock>
-          <TeamMemberBlock name="Joe Leo" linkTo="joe-leo" role="Ceo/Co-founder"></TeamMemberBlock>
-          <TeamMemberBlock name="Joe Leo" linkTo="joe-leo" role="Ceo/Co-founder"></TeamMemberBlock>
-          <TeamMemberBlock name="Joe Leo" linkTo="joe-leo" role="Ceo/Co-founder"></TeamMemberBlock>
-          <TeamMemberBlock name="Joe Leo" linkTo="joe-leo" role="Ceo/Co-founder"></TeamMemberBlock>
-          <TeamMemberBlock name="Joe Leo" linkTo="joe-leo" role="Ceo/Co-founder"></TeamMemberBlock>
-          <TeamMemberBlock name="Joe Leo" linkTo="joe-leo" role="Ceo/Co-founder"></TeamMemberBlock>
-          <TeamMemberBlock name="Joe Leo" linkTo="joe-leo" role="Ceo/Co-founder"></TeamMemberBlock>
-          <TeamMemberBlock name="Joe Leo" linkTo="joe-leo" role="Ceo/Co-founder"></TeamMemberBlock>
-          <TeamMemberBlock name="Joe Leo" linkTo="joe-leo" role="Ceo/Co-founder"></TeamMemberBlock>
-          <TeamMemberBlock name="Joe Leo" linkTo="joe-leo" role="Ceo/Co-founder"></TeamMemberBlock>
-          <TeamMemberBlock name="Joe Leo" linkTo="joe-leo" role="Ceo/Co-founder"></TeamMemberBlock>
-          <TeamMemberBlock name="Joe Leo" linkTo="joe-leo" role="Ceo/Co-founder"></TeamMemberBlock>
-          <TeamMemberBlock name="Joe Leo" linkTo="joe-leo" role="Ceo/Co-founder"></TeamMemberBlock>
-          <TeamMemberBlock name="Joe Leo" linkTo="joe-leo" role="Ceo/Co-founder"></TeamMemberBlock>
-          <TeamMemberBlock name="Joe Leo" linkTo="joe-leo" role="Ceo/Co-founder"></TeamMemberBlock>
-          <TeamMemberBlock name="Joe Leo" linkTo="joe-leo" role="Ceo/Co-founder"></TeamMemberBlock>
-          <TeamMemberBlock name="Joe Leo" linkTo="joe-leo" role="Ceo/Co-founder"></TeamMemberBlock>
-
-
-        </div>
-      </OurTeamSection>
-
-      <WhatWeveBuiltSection>
-
+      {/* <WhatWeveBuiltSection>
         <h1>What We've Built</h1>
-        <p>You don't have to take our word for it that we can build great software- just take a look at some the awesome project we've made!</p>
-      </WhatWeveBuiltSection>
+        <p>
+          You don't have to take our word for it that we can build great
+          software- just take a look at some the awesome project we've made!
+        </p>
+      </WhatWeveBuiltSection> */}
 
       <br />
       <br />
       <br />
       <br />
 
-      <PinkBoxesContainer>
-
-
+      {/* <PinkBoxesContainer>
         <Link to="/our-work">
           <PinkBox>
             <h1>Previous Success Stories</h1>
             <p>Real case stories of businesses we've helped.</p>
           </PinkBox>
-        </Link>
+        </Link> */}
 
-        <Link to="/our-work">
+        {/* <Link to="/our-work">
           <PinkBox>
             <h1>Open Source</h1>
             <p>See how we contribute back to the software comunity.</p>
           </PinkBox>
         </Link>
-      </PinkBoxesContainer>
+      </PinkBoxesContainer> */}
       <br />
       <br />
       <br />
-
-
 
       <JoinOurTeamCornerBtn />
       {/* <h1>Some of Our Partners</h1>
@@ -416,8 +458,6 @@ const OurWorkPage = () => (
       <br />
       <br />
     </Layout>
-
-
   </Global>
 );
 
