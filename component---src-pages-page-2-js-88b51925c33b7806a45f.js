1e24039d0a76aa679c792dccfce64cb0ab9415f3
JimLynchCodes <mrdotjim@gmail.com>
2023-07-16 12:24:06 -0400

    Updates

---
+++ component---src-pages-page-2-js-88b51925c33b7806a45f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[19],{p5nM:function(e,t,n){"use strict";n.r(t);n("q1tI");var o=n("Wbzz"),b=n("Bl7J"),c=n("vrFN"),a=n("qKvR");t.default=function(){return Object(a.b)(b.a,null,Object(a.b)(c.a,{title:"Page two"}),Object(a.b)("h1",null,"Hi from the second page"),Object(a.b)("p",null,"Welcome to page 2"),Object(a.b)(o.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-88b51925c33b7806a45f.js.map
